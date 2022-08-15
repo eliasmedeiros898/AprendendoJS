6617818f1db0ae49b64931c4cdbe198642572b02
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2022-08-15 15:32:45 -0300

    Commit aula 11

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A sua velocidade é ${vel} Km/h`)
+if(vel > 60){
+    console.log(`Você está acima do limite de velocidade permitido`)
+}
+console.log("Dirija sempre com segurança!")
