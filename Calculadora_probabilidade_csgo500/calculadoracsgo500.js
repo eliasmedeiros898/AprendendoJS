ae6a9241d9050d6a8683a41f9edb9e6fc61391b3
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2022-08-24 18:27:53 -0300

    melhorias no codigo e avanço no html

--- Calculadora_probabilidade_csgo500/calculadoracsgo500.js
+++ Calculadora_probabilidade_csgo500/calculadoracsgo500.js
@@ -1,6 +1,6 @@
 
-var init_bet = 15
-var saldo_ini = 16374
+var init_bet = 50
+var saldo_ini = 83000
 var num = 10
 var saldo = saldo_ini
 
@@ -12,7 +12,7 @@ function arredondar(n){
 
 function calcular(num, saldo, init_bet, resultado){
     var cont = 1
-    total_apostado = init_bet
+    var total_apostado = init_bet
     const mult = 2.1
     
     for(var i=1;i<num;i++){
@@ -29,10 +29,10 @@ console.log(`Total apostado: ${arredondar(total_apostado)}`)
 
 if(saldo>=0){
     console.log(`Saldo restante: ${arredondar(saldo)}`)
-    console.log(`Saldo recomendado para a ${cont+1} bet: ${arredondar(init_bet*mult+saldo)}`)
+    console.log(`Saldo recomendado para chegar na ${cont+1} bet: ${arredondar(total_apostado*mult)}`)
 }else{
     console.log(`Saldo que falta para chegar nessa aposta: ${arredondar(total_apostado - saldo_ini)}`)
-    console.log(`Saldo recomendado para a ${cont+1} bet: ${arredondar(total_apostado + (init_bet*mult))}`)
+    console.log(`Saldo recomendado para chegar na ${cont+1} bet: ${arredondar(total_apostado *mult)}`)
 }
 }
     
